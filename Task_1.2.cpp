6a1e20d30b2ade1ca9cf720d2f81f9ad3989fed2
Telkarr <129428957+Telkarr@users.noreply.github.com>
2023-11-03 02:58:15 +0600

    Add files via upload

---
+++ Task_1.2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int removeElementInplace(vector<int>& arr, int val) {
+    int n = arr.size();
+    int j = 0;  // Индекс для нового массива
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != val) {
+            arr[j] = arr[i];
+            j++;
+        }
+    }
+    return j;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    vector<int> arr = { 0,2,6,6,1,0,4,6 };
+    int val1 = 6;
+    int newSize1 = removeElementInplace(arr, val1);
+
+    cout << "результат: " << newSize1 << ", arr = [";
+    for (int i = 0; i < arr.size(); i++) {
+        if (i < newSize1) {
+            cout << arr[i];
+        }
+        else {
+            cout << "_";
+        }
+
+        if (i < arr.size() - 1) {
+            cout << ",";
+        }
+    }
+    cout << "]" << endl;
+    return 0;
+}
+
+
+
+
+
---
+++ Task_1.3.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool hasDuplicates(vector<int>& arr) {
+    sort(arr.begin(), arr.end());
+    int n = arr.size();
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] == arr[i - 1]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    vector<int> arr = { 4,2,7,4,9,1,5 };
+    int n = arr.size();
+
+    cout << "Массив: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i];
+        if (i < n - 1) {
+            cout << ",";
+        }
+    }
+    cout << endl;
+
+    if (hasDuplicates(arr)) {
+        for (int i = 1; i < n; i++) {
+            if (arr[i] == arr[i - 1]) {
+                cout << "Дубликат числа " << arr[i] << endl;
+            }
+        }
+    }
+    else {
+        cout << "Нет дубликатов" << endl;
+    }
+
+    return 0;
+}

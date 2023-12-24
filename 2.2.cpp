d7732ad02df41956da70c54dfc8f09da92b77933
Георгий <129428957+Telkarr@users.noreply.github.com>
2023-12-24 12:22:41 +0600

    2.2.cpp

--- 2.2.cpp
+++ 2.2.cpp
@@ -2,13 +2,11 @@
 #include <vector>
 using namespace std;
 
-//объединение
 void merge(vector<int> &arr, vector<int> &buffer, size_t begin, size_t mid, size_t end) {
     size_t left = begin;
     size_t right = mid + 1;
     size_t temp = begin;
-
-    // Слияние двух подмассивов во временный буфер
+    
     while (left <= mid && right <= end) {
         if (arr[left] <= arr[right]) {
             buffer[temp++] = arr[left++];
@@ -17,17 +15,14 @@ void merge(vector<int> &arr, vector<int> &buffer, size_t begin, size_t mid, size
         }
     }
 
-    // Копирование оставшихся элементов из левого массива
     while (left <= mid) {
         buffer[temp++] = arr[left++];
     }
 
-    // Копирование из правого
     while (right <= end) {
         buffer[temp++] = arr[right++];
     }
 
-    // Копирование объединённых элементов из буфера обратно в исходный массив
     for (size_t i = begin; i <= end; i++) {
         arr[i] = buffer[i];
     }
@@ -46,7 +41,6 @@ void merge_sort(vector<int> &arr, vector<int> &buffer, size_t begin, size_t end)
     merge(arr, buffer, begin, mid, end);
 }
 
-//интерфейс
 void run_merge_sort(vector<int> &arr) {
     vector<int> buffer(arr.size());
     merge_sort(arr, buffer, 0, arr.size() - 1);

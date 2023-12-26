2712f529223c0fc60a112b95d491f7fea2f36b10
Георгий <129428957+Telkarr@users.noreply.github.com>
2023-12-26 13:22:51 +0600

    7.1.cpp

--- 7.1.cpp
+++ 7.1.cpp
@@ -3,32 +3,41 @@
 
 void start(const std::vector<int> &field, std::size_t n, std::size_t m) {
   std::vector<std::vector<int>> dp(n, std::vector<int>(m, 0));
-  std::vector<std::vector<std::string>> path(n,std::vector<std::string>(m, ""));
 
   dp[0][0] = field[0];
   for (std::size_t i = 1; i < n; i++) {
     dp[i][0] = dp[i - 1][0] + field[i * m];
-    path[i][0] = path[i - 1][0] + "D";
   }
   for (std::size_t j = 1; j < m; j++) {
     dp[0][j] = dp[0][j - 1] + field[j];
-    path[0][j] = path[0][j - 1] + "R";
   }
 
   for (std::size_t i = 1; i < n; i++) {
     for (std::size_t j = 1; j < m; j++) {
       if (dp[i - 1][j] > dp[i][j - 1]) {
         dp[i][j] = dp[i - 1][j] + field[i * m + j];
-        path[i][j] = path[i - 1][j] + "D";
       } else {
         dp[i][j] = dp[i][j - 1] + field[i * m + j];
-        path[i][j] = path[i][j - 1] + "R";
       }
     }
   }
 
   std::cout << "Max coins: " << dp[n - 1][m - 1] << "\n";
-  std::cout << "Path: " << path[n - 1][m - 1] << "\n";
+
+  // Восстанавливаем путь
+  std::size_t i = n - 1, j = m - 1;
+  std::string result_path = "";
+  while (i > 0 || j > 0) {
+    if (i > 0 && dp[i][j] - field[i * m + j] == dp[i - 1][j]) {
+      result_path = 'D' + result_path;
+      i--;
+    } else {
+      result_path = 'R' + result_path;
+      j--;
+    }
+  }
+
+  std::cout << "Path: " << result_path << "\n";
 }
 
 int main() {
